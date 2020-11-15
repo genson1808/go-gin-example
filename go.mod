ef59502b85a2561bc036a8fab5fe9468adab1736
rogger1808 <genson1808@gmail.com>
2020-11-15 21:53:05 +0700

    update graceful restart/shutdown

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.15
 require (
 	github.com/asaskevich/govalidator v0.0.0-20200907205600-7a23bdc65eef // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6 // indirect
+	github.com/fvbock/endless v0.0.0-20170109170031-447134032cb6
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-ini/ini v1.62.0
 	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible
--- main.go
+++ main.go
@@ -4,19 +4,57 @@ import (
 	"fmt"
 	"github.com/ROGGER1808/go-gin-example/pkg/setting"
 	"github.com/ROGGER1808/go-gin-example/routers"
-	"net/http"
+	"github.com/fvbock/endless"
+	"log"
+	"syscall"
 )
 
 func main() {
-	router := routers.InitRouter()
 
-	s := &http.Server{
-		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
-		Handler:        router,
-		ReadTimeout:    setting.ReadTimeout,
-		WriteTimeout:   setting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
+	// Graceful restart
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
+
+	server := endless.NewServer(endPoint, routers.InitRouter())
+	server.BeforeBegin = func(add string) {
+		log.Printf("Actual pid is %d", syscall.Getpid())
 	}
 
-	s.ListenAndServe()
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Printf("Server err: %v", err)
+	}
+
+	//Graceful shutdown
+	//router := routers.InitRouter()
+	//
+	//s := &http.Server{
+	//	Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+	//	Handler:        router,
+	//	ReadTimeout:    setting.ReadTimeout,
+	//	WriteTimeout:   setting.WriteTimeout,
+	//	MaxHeaderBytes: 1 << 20,
+	//}
+	//
+	//go func() {
+	//	if err := s.ListenAndServe(); err != nil {
+	//		log.Printf("Listen: %s\n", err)
+	//	}
+	//}()
+	//
+	//quit := make(chan os.Signal)
+	//signal.Notify(quit, os.Interrupt)
+	//<- quit
+	//
+	//log.Println("Shutdown Server ...")
+	//
+	//ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	//defer cancel()
+	//if err := s.Shutdown(ctx); err != nil {
+	//	log.Fatal("Server Shutdown:", err)
+	//}
+	//
+	//log.Println("Server exiting")
 }
